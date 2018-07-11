eefdc6a5686fe317bb02171d75476060022bb11c
Student <student@EPBYMINW1374.minsk.epam.com>
2018-07-11 11:32:06 +0300

    changes in task3

--- task3.py
+++ task3.py
@@ -1,7 +1,9 @@
 N = int(input("Enter N = "))
-
 if N == 0 or N == 1 or N >= 10 ** 9:
     print("Q = -1")
+    exit(0)
+if N < 10:
+    print("Q = ", N + 10)
     exit(0)
 reslist = []
 for i in range(9, 1, -1):
@@ -14,4 +16,4 @@ if N > 10:
 n = reslist[len(reslist) - 1]
 for i in range(len(reslist) - 2, -1, -1):
     n = 10 * n + reslist[i]
-print(n)
+print("Q = ", n)
