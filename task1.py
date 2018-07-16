c41907b374e9a5078181b967fd6ee872973e7f8f
Student <student@EPBYMINW1374.minsk.epam.com>
2018-07-16 10:40:17 +0300

    editing task1

--- task1.py
+++ task1.py
@@ -1,11 +1,6 @@
 def is_leap_year():
-    if 1900 <= year <= 10 ** 5 and year % 4 == 0 and \
-            (year % 100 != 0 or year % 400 == 0):
-        leap = True
-        return leap
-    else:
-        leap = False
-        return leap
+    return (1900 <= year <= 10 ** 5 and year % 4 == 0 and
+            (year % 100 != 0 or year % 400 == 0))
 
 
 year = int(input("Enter the year: "))
